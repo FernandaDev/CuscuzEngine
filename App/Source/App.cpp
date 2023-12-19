782a2a5f2d083dd9f0ac8c659ba7076b2c4d53f6
Fernanda Oliveira <fernanda.oliveira@kokku.local>
2023-12-18 22:41:23 -0300

    Setup SDL; Created a window;

--- App/Source/App.cpp
+++ App/Source/App.cpp
@@ -1,6 +1,50 @@
-#include "Core/Core.h"
+#include <iostream>
 
-int main()
+#include <SDL.h>
+
+#define SCREEN_WIDTH   1280
+#define SCREEN_HEIGHT  720
+
+int main(int agrc, char* argv[])
 {
-	Core::PrintHelloWorld();
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	{
+		std::cout << "SDL could not be initialized: " << SDL_GetError() << std::endl;
+		return 0;
+	}
+
+	SDL_Window* window = SDL_CreateWindow("Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+	if (!window)
+	{
+		std::cout << "Could not create a window." << std::endl;
+		return 0;
+	}
+
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+	if (!renderer)
+	{
+		std::cout << "Could not create a renderer." << std::endl;
+		return 0;
+	}
+
+	bool gameIsRunning = true;
+	while (gameIsRunning)
+	{
+		SDL_Event event;
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+				gameIsRunning = false;
+		}
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+
+	SDL_Quit();
+
+	return 0;
 }
